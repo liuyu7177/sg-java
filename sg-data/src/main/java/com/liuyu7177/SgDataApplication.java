c329e71da6df4f9fe82ce397bf4e2d4c18b0988f
liuyu7177 <853469204@qq.com>
2019-10-28 14:30:27 +0800

    123

--- sg-data/src/main/java/com/liuyu7177/SgDataApplication.java
+++ sg-data/src/main/java/com/liuyu7177/SgDataApplication.java
@@ -2,8 +2,9 @@ package com.liuyu7177;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class SgDataApplication {
 
     public static void main(String[] args) {
